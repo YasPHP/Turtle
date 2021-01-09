71af45e97ca52440777d64b52b946de594cc2188
Mishaal Farooq <69223822+mishaalfarooq99@users.noreply.github.com>
2021-01-09 17:26:01 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div>
+            <h1>Hello Ninjas. Welcome to the app!!</h1>
+        </div>
+    );
+}
+export default App; //hey guys
