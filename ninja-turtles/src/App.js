4c44b0b3f81aba75ad27b89d3ce6045dfec90299
mishaalfarooq99 <69223822+mishaalfarooq99@users.noreply.github.com>
2021-01-10 01:38:05 -0500

    Update App.js

--- ninja-turtles/src/App.js
+++ ninja-turtles/src/App.js
@@ -5,8 +5,7 @@ function App(){
         <div>
             <style>{'body { background-color: papayawhip; }'}</style>
             <h1 style={{ color: 'green' }}>NINJA TURTLE HANGOUT</h1>
-            <h2>Welcome. This is a web app designed to protect children's video identities using fun filters to protect them. It includes features such as facial detection and generating a random username. </h2>
-            
+            <h2>Welcome! This is a web app designed to protect children's video identities using fun filters to protect them. It includes features such as facial detection and adding a Ninja Turtle filter.</h2>
         </div>
     );
 }
