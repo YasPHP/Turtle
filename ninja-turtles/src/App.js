db4f218d6a46ff92e5a083d090b0fbe3f8faca41
mishaalfarooq99 <69223822+mishaalfarooq99@users.noreply.github.com>
2021-01-09 20:31:23 -0500

    Added description + colour changes

---
+++ ninja-turtles/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div>
+            <style>{'body { background-color: papayawhip; }'}</style>
+            <h1 style={{ color: 'green' }}>NINJA TURTLE HANGOUT</h1>
+            <h2>Welcome. This is a web app designed to protect children's video identities using fun filters to protect them. It includes features such as facial detection and generating a random username. </h2>
+            
+        </div>
+    );
+}
+export default App;
