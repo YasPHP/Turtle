ed0f3752c71839135d16a2a7439f1043574db588
Yasmeen Brain <47681976+YasPHP@users.noreply.github.com>
2021-01-14 00:47:18 -0500

    face_cascade's  detectMultiScale() algorithm bio + keys output

--- FacialDetection.py
+++ FacialDetection.py
@@ -37,25 +37,35 @@ while(1):
     # colour converted from red, green, blue to gray
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # 
+    # receives a frame (img) as an argument and runs the classifier cascade over the image. 
+    # the algorithm looks at subregions of the image in multiple scales, to detect faces of varying sizes.
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     # face tracking loop
     for (x,y,w,h) in faces:
         
+        # adds rectangle around face tracker
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+        
+        # the text displayed on top of the framed face's rectangle
         cv2.putText(img,"Face Detected",(x,y),1,1,(0,255,0),2)
         
+        # image is overlayed on top of face tracker rectangle
         try: img[y:y+overlay.shape[0], x:x+overlay.shape[1]] = overlay
+          
+        # otherwise, no overlay is placed
         except: continue
         
-
+    # shows the face cover on webcam stream
     cv2.imshow('Face Cover',img)
     
-    # spacebar command to alternate face filters
+    # the keyboard key in question
     k = cv2.waitKey(30) & 0xff
+    
+    # escape button to close window
     if k == 27:
         break
+    # spacebar command to alternate face filters
     if k == 32:
         if chosen_face == len(face_list) - 1:
             chosen_face = 0
